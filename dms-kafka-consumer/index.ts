22e28c69471a22413fedca881cf5765ab6e9edac
Takashi Ikki <new_ittyandesu@outlook.jp>
2022-02-10 00:54:12 +0900

    docker 環境からなら接続出来る事を確認

--- dms-kafka-consumer/index.ts
+++ dms-kafka-consumer/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+import kafka from "kafka-node";
+
+const client = new kafka.KafkaClient({
+  kafkaHost: "kafka:9092",
+  connectTimeout: 3000,
+  requestTimeout: 3000,
+});
+
+const consumer = new kafka.Consumer(
+  client,
+  [{ topic: "dms-local.ms_dms.user" }],
+  {
+    autoCommit: true,
+    fromOffset: true,
+    groupId: "consumer1",
+  }
+);
+
+consumer.on("message", function (message) {
+  console.log(message);
+});
+
+console.log("ready.");
